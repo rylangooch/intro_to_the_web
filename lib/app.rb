99a6da447be44e739d4e6bdee632e5604f805884
Rylan Gooch <rylangooch@gmail.com>
2016-07-18 16:33:45 +0100

    Challenge 12

--- lib/app.rb
+++ lib/app.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/cat' do
+get '/random-cat' do
   @sample_name = ['Amigo', 'Oskar', 'Viking'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @sample_name = params[:name]
   erb(:index)
 end
