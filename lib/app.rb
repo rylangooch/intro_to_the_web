8baa18b229c2521a9cd887d5b62e266835a592ec
yurizhuravel <zhuravel@gmail.com>
2016-07-18 16:58:02 +0100

    Adds index.erb

--- lib/app.rb
+++ lib/app.rb
@@ -12,7 +12,5 @@ get('/foo') do
 end
 
 get '/cat' do
-  "<body style='border: 500px dashed black'>
-    <img style='border: thick solid pink', src= 'http://bit.ly/1eze8aE'>
-  </body>"
+  erb(:index)
 end
