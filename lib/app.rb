201cd58076be29b3b5144c3623ff9d5bb4953d20
Rylan Gooch <rylangooch@gmail.com>
2016-07-19 12:18:55 +0100

    Challenge 15

--- lib/app.rb
+++ lib/app.rb
@@ -16,8 +16,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
-  p params
+post '/named-cat' do
+p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
